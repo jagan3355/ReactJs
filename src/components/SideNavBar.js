9a0f31c272cf11b18347da8bb9683eacb33a2513
jagan3355 <jagan786jagan@gmail.com>
2019-02-10 23:08:13 +0530

    Add files via upload

---
+++ src/components/SideNavBar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+class SideNavBar extends React.Component{
+
+render()
+{
+    return(
+<div class="container">
+<div class="row">
+<div class="col-md-2">
+<ul>
+<li>
+    1
+</li>
+<li>
+    1
+</li>
+<li>
+    1
+</li>
+<li>
+    1
+</li>
+
+
+</ul>
+
+
+
+</div>
+</div>
+
+
+</div>
+
+
+    );
+
+}
+
+
+
+}
+export default SideNavBar;
